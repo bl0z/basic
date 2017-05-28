e188d1fa405d722fb1169b7617ef537f6ceeef1d
Victor Cortés <victor.aljamar@gmail.com>
2017-05-28 14:58:21 +0200

    test

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -123,7 +123,7 @@ class SiteController extends Controller
 //        return $this->render('about');
         try {
             $info = json_decode(base64_decode($get));
-            echo $info->apellidos;
+            echo $info->apellidos . " development";
         }catch(\yii\base\ErrorException $e){
             return $this->render('error', array("name"=>"Invalid JSON","message"=>"Invalid JSON"));
         }
